36ae6915515fcfc13ae41def3f3d0c89630a47f7
fedora Cloud User <fedora@ip-172-30-0-179.ec2.internal>
2018-06-20 23:17:37 +0000

    wip

--- core/daemon/daemon.go
+++ core/daemon/daemon.go
@@ -2,29 +2,38 @@ package daemon
 
 import (
 	"fmt"
+	"log"
+	"path/filepath"
 
+	"github.com/docker/docker/daemon"
 	"github.com/docker/docker/daemon/config"
+	"github.com/docker/docker/libcontainerd"
+	"github.com/docker/docker/registry"
 )
 
 // Daemon ...
 type Daemon struct {
-	daemon *docker.Daemon
+	daemon *daemon.Daemon
 }
 
 // New ...
 func New() *Daemon {
 	cfg := config.New()
 	fmt.Println(cfg)
-	/*
-		registryService, err := registry.NewService()
-		if err != nil {
-			return err
-		}
+	registryService, err := registry.NewService(registry.ServiceOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		containerdRemote, err := libcontainerd.New(filepath.Join("/", "containerd"), filepath.Join("/", "containerd"))
-		if err != nil {
-			return err
-		}
+	fmt.Println(registryService)
+
+	containerdRemote, err := libcontainerd.New(filepath.Join("/tmp/", "containerd"), filepath.Join("/tmp/", "containerd"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(containerdRemote)
+	/*
 
 		pluginStore := plugin.NewStore()
 
@@ -35,6 +44,6 @@ func New() *Daemon {
 	*/
 
 	return &Daemon{
-	//daemon: daemon,
+		//daemon: daemon,
 	}
 }
