0205599a51cdb73d30d678659dbaf4a99db8f0f7
Miguel Mota <miguelmota2@gmail.com>
2018-06-22 16:14:02 -0700

    faketime start

--- snapshot_test/index.js
+++ snapshot_test/index.js
@@ -1,10 +1,6 @@
 var state = {}
 
-console.log("unmodified time", new Date())
-
-process.env['FAKETIME'] = '2017-01-01 00:00:00'
-
-console.log("modified time", new Date())
+console.log('modified time', new Date())
 
 setTimeout(() => {
   state['foo'] = 'bar'
