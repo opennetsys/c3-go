66e9319eff1f53694aaa8f6b8168e2c4a285228b
Adam Hanna <ahanna@alumni.mines.edu>
2018-07-17 12:10:36 -0700

    added peer discovery; added dht routing

--- node/start.go
+++ node/start.go
@@ -52,7 +52,14 @@ type DiscoveryNotifee struct {
 
 // HandlePeerFound ...
 func (n *DiscoveryNotifee) HandlePeerFound(pi peerstore.PeerInfo) {
-	n.h.Connect(context.Background(), pi)
+	n.h.Peerstore().AddAddrs(pi.ID, pi.Addrs, peerstore.PermanentAddrTTL)
+	if err := n.h.Connect(context.Background(), pi); err != nil {
+		log.Printf("[node] found peer %s\nerr connecting %v", pi.Addrs, err)
+
+		return
+	}
+
+	log.Printf("[node] found peer %s\nadded to peerstore and connected", pi.Addrs)
 }
 
 // Start ...
